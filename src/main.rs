f8818bd7dd54defe9762451b3c2640f9549da5e7
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2024-03-11 15:09:16 -0300

    Add parsing and messaging

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ extern crate netxt;
 use std::{error, result};
 use tokio::time::{sleep, Duration};
 
-use netxt::Todo;
+use netxt::{Day, Todo};
 
 use teloxide::prelude::*;
-use teloxide::types::Recipient::Id;
+use teloxide::types::{
+    MediaKind::Text,
+    MessageKind::Common,
+    Recipient::Id,
+    UpdateKind::{EditedMessage, Message},
+};
 
 static TODO_FILE: &str = "todo.txt";
 static CHAT_ID: ChatId = ChatId(26289585); // only work with a single user
@@ -19,21 +24,50 @@ async fn main() -> Result<()> {
 
     let bot = Bot::from_env();
 
-    // new todo
-    let mut _todo = Todo::new(Some(TODO_FILE));
+    let mut todo = Todo::new(Some(TODO_FILE))?;
+
+    // TODO: command to force save a day
+    // TODO: /stop command
+    // TODO: /start command
+    // TODO: maybe make this into a systemd thing
 
     loop {
         // print new today message
-        let _ = bot
-            .send_message(Id(CHAT_ID), format!("Today Message goes here").to_string())
+        let msg = bot
+            .send_message(Id(CHAT_ID), format!("{}", todo.today).to_string())
             .await?;
+        // TODO: button to copy day
 
-        sleep(Duration::from_secs(5)).await;
-        // sleep 1 day
+        sleep(Duration::from_secs(15)).await;
+        // TODO: sleep 1 day
         // sleep(Duration::from_secs(86400)).await;
-        // parse today message
-        // save today as yesterday
-        break;
+
+        let updates = bot.get_updates().await?;
+        if updates.len() != 0 {
+            match &updates[updates.len() - 1].kind {
+                // use last update
+                Message(msg) | EditedMessage(msg) => {
+                    if let Common(msg_common) = &msg.kind {
+                        if let Text(txt) = &msg_common.media_kind {
+                            println!("{}", txt.text);
+                            let last_day: Day = txt.text.parse()?;
+                            // ignore if date is incorrect (maybe from past update)
+                            if last_day.date == todo.today.date {
+                                todo.today = last_day;
+                            }
+                        }
+                    }
+                }
+                _ => {
+                    println!("nothing to update today");
+                }
+            }
+        }
+
+        // save today and create new day
+        // create new today
+        // TODO: next day should have all undone tasks of current day
+        todo.next_day();
     }
-    Ok(())
+    // Ok(())
 }
