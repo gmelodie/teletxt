d0ff511d8fe705316e1aedbb3b400d1dc50307eb
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2024-03-12 18:43:53 -0300

    Make into idempotent daemon thing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 extern crate netxt;
 use std::{error, result};
-use tokio::time::{sleep, Duration};
 
-use netxt::{Day, Todo};
+use netxt::{today, Day, Todo};
 
 use teloxide::prelude::*;
 use teloxide::types::{
@@ -15,6 +14,10 @@ use teloxide::types::{
 static TODO_FILE: &str = "todo.txt";
 static CHAT_ID: ChatId = ChatId(26289585); // only work with a single user
 
+macro_rules! err {
+    ($($tt:tt)*) => { Err(Box::<dyn error::Error>::from(format!($($tt)*))) };
+}
+
 type Result<T> = result::Result<T, Box<dyn error::Error>>;
 
 #[tokio::main]
@@ -23,51 +26,75 @@ async fn main() -> Result<()> {
     log::info!("Starting netxt bot...");
 
     let bot = Bot::from_env();
-
     let mut todo = Todo::new(Some(TODO_FILE))?;
 
     // TODO: command to force save a day
     // TODO: /stop command
     // TODO: /start command
     // TODO: maybe make this into a systemd thing
+    // TODO: button to copy day
 
-    loop {
-        // print new today message
-        let msg = bot
-            .send_message(Id(CHAT_ID), format!("{}", todo.today).to_string())
-            .await?;
-        // TODO: button to copy day
+    let mut today_present = false;
+    if todo.today.sections.len() != 0 {
+        today_present = true;
+    }
 
-        sleep(Duration::from_secs(15)).await;
-        // TODO: sleep 1 day
-        // sleep(Duration::from_secs(86400)).await;
+    let updates = bot.get_updates().await?;
+    let mut last_update_id = -1;
 
-        let updates = bot.get_updates().await?;
-        if updates.len() != 0 {
-            match &updates[updates.len() - 1].kind {
-                // use last update
-                Message(msg) | EditedMessage(msg) => {
-                    if let Common(msg_common) = &msg.kind {
-                        if let Text(txt) = &msg_common.media_kind {
-                            println!("{}", txt.text);
-                            let last_day: Day = txt.text.parse()?;
-                            // ignore if date is incorrect (maybe from past update)
-                            if last_day.date == todo.today.date {
-                                todo.today = last_day;
-                            }
-                        }
-                    }
-                }
-                _ => {
-                    println!("nothing to update today");
-                }
+    for u in updates {
+        if let Ok(day) = is_day_msg(&u) {
+            if day.date == today() {
+                todo.today = day;
+                today_present = true;
+            } else if !todo.days.contains(&day) && day.date < today() {
+                todo.days.push(day);
+            } else {
+                return err!("wrong date");
             }
         }
-
-        // save today and create new day
-        // create new today
-        // TODO: next day should have all undone tasks of current day
-        todo.next_day();
+        last_update_id = u.id;
     }
-    // Ok(())
+
+    if last_update_id != -1 {
+        // ack update
+        bot.get_updates()
+            .offset(last_update_id + 1)
+            .send()
+            .await
+            .unwrap();
+    }
+
+    if !today_present {
+        todo.next_day();
+        print_day_msg(bot, &todo.today).await?;
+    }
+    todo.save()?;
+
+    Ok(())
+}
+fn is_day_msg(update: &Update) -> Result<Day> {
+    match &update.kind {
+        // use last update
+        Message(msg) | EditedMessage(msg) => {
+            if let Common(msg_common) = &msg.kind {
+                if let Text(txt) = &msg_common.media_kind {
+                    println!("{}", txt.text);
+                    let day: Day = txt.text.parse()?;
+                    return Ok(day);
+                }
+            }
+            err!("Update is not Message or EditedMessage")
+        }
+        _ => {
+            err!("nothing to update today")
+        }
+    }
+}
+
+async fn print_day_msg(bot: Bot, day: &Day) -> Result<()> {
+    let _msg = bot
+        .send_message(Id(CHAT_ID), format!("{}", day).to_string())
+        .await?;
+    Ok(())
 }
