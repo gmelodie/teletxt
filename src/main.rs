650f10edb048ea26f4974935f6a11f0d02b6b072
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2024-03-21 15:30:52 -0300

    Add multi user allow list

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 extern crate netxt;
 use std::{
-    error, fmt,
+    error, fmt, fs,
     io::{stdout, Write},
+    path::Path,
     result,
 };
 
@@ -10,15 +11,11 @@ use netxt::{today, Day, Todo};
 use teloxide::payloads::GetUpdatesSetters;
 use teloxide::prelude::*;
 use teloxide::types::{
-    AllowedUpdate,
-    MediaKind::Text,
-    MessageKind::Common,
-    Recipient::Id,
-    UpdateKind::{EditedMessage, Message},
+    AllowedUpdate, MediaKind::Text, Message, MessageKind::Common, UpdateKind, User,
 };
 
-static TODO_FILE: &str = "todo.txt";
-static CHAT_ID: ChatId = ChatId(26289585); // only work with a single user
+static TODO_DIR: &str = "todos";
+static ALLOWED_USERS_FILE: &str = "allowed-users.txt";
 
 macro_rules! err {
     ($($tt:tt)*) => { Err(Box::<dyn error::Error>::from(format!($($tt)*))) };
@@ -53,30 +50,11 @@ async fn main() -> Result<()> {
     log::info!("Starting netxt bot...");
 
     let bot = Bot::from_env();
-    let mut todo = Todo::new(Some(TODO_FILE))?;
 
     // TODO: command to force save a day (use update_todo)
     // TODO: maybe make this into a systemd thing
     // TODO: button to copy day
 
-    update_todo(&bot, &mut todo).await?; // TODO: use NoUpdates error
-
-    if todo.days.len() != 0 {
-        let last_day = todo.days.iter().max_by_key(|d: &&Day| d.date).unwrap();
-        if last_day.date < today() {
-            todo.next_day();
-            print_day_msg(bot, &todo.today).await?;
-        }
-    } else {
-        todo.next_day();
-        print_day_msg(bot, &todo.today).await?;
-    }
-
-    todo.save()?;
-    Ok(())
-}
-
-async fn update_todo<'a>(bot: &Bot, todo: &mut Todo<'a>) -> Result<()> {
     let updates = bot
         .get_updates()
         .allowed_updates([AllowedUpdate::Message, AllowedUpdate::EditedMessage])
@@ -84,26 +62,19 @@ async fn update_todo<'a>(bot: &Bot, todo: &mut Todo<'a>) -> Result<()> {
     if updates.len() == 0 {
         return Err(From::from(UpdateError::NoUpdates));
     }
+
     let mut last_update_id = -1;
 
     for u in updates {
-        last_update_id = u.id;
-        if let Ok(day) = is_day_msg(&u) {
-            println!("{day}");
-            if day.date == today() {
-                todo.today = day;
-            } else if !todo.days.contains(&day) && day.date < today() {
-                todo.days.push(day);
-            } else {
-                // ack last processed update
-                bot.get_updates()
-                    .offset(last_update_id + 1)
-                    .send()
-                    .await
-                    .unwrap();
-                return err!("Unable to update a previous day that is already registered");
+        println!("{:#?}", u);
+        if let Ok((username, day)) = is_valid_update(&u) {
+            if let Err(err) = update_todo(&username, &day).await {
+                // TODO: use NoUpdates error
+                writeln!(stdout(), "{err}, leaving update {} without ack", u.id)?;
+                continue; // this makes it so that the update is not acked (we want to leave it to retry since it seems to be a valid day if it got here)
             }
         }
+        last_update_id = u.id;
     }
 
     if last_update_id != -1 {
@@ -112,21 +83,119 @@ async fn update_todo<'a>(bot: &Bot, todo: &mut Todo<'a>) -> Result<()> {
             .offset(last_update_id + 1)
             .send()
             .await
-            .unwrap();
+            .expect("Unable to ack last_update_id={last_update_id}");
     }
 
     Ok(())
 }
 
-fn is_day_msg(update: &Update) -> Result<Day> {
-    match &update.kind {
-        // use last update
-        Message(msg) | EditedMessage(msg) => {
-            if let Common(msg_common) = &msg.kind {
-                if let Text(txt) = &msg_common.media_kind {
-                    let day: Day = txt.text.parse()?;
-                    return Ok(day);
+async fn update_todo(username: &str, day: &Day) -> Result<()> {
+    // load todo from file or create if doesnt exist
+    // ./TODO_DIR/{username}.txt
+    let file_path = Path::new(TODO_DIR)
+        .join(format!("{username}.txt"))
+        .display()
+        .to_string();
+    let mut todo = Todo::new(Some(&file_path))?;
+
+    // if user is not allowed, ignore this update
+    if !allowed_user(username)? {
+        return err!("Username not whitelisted");
+    }
+
+    println!("Saving... {day}");
+    if day.date == today() {
+        todo.today = day.clone();
+    } else if !todo.days.contains(&day) && day.date < today() {
+        todo.days.push(day.clone());
+    } else {
+        return err!("Unable to update a previous day that is already registered");
+    }
+    // TODO: fix error saving twice (think its here) get rid of the today bullshit
+    if todo.days.len() == 0 {
+        todo.next_day();
+    } else {
+        let last_day = todo.days.iter().max_by_key(|d: &&Day| d.date).unwrap();
+        if last_day.date < today() {
+            todo.next_day();
+        }
+    }
+
+    todo.save()?;
+
+    Ok(())
+}
+
+/// Validates and, if valid, parses a Day from an Update message
+// TODO: maybe this could be a From trait implementation
+fn is_valid_update(update: &Update) -> Result<(String, Day)> {
+    // unwrap msg
+    let msg = get_msg(update)?;
+
+    // unwrap user
+    let user = get_user(&msg)?;
+
+    // unwrap day
+    let day = is_day_msg(msg)?;
+
+    // get username
+    if let Some(username) = user.username {
+        Ok((username, day))
+    } else {
+        err!("No username found in message")
+    }
+}
+
+fn allowed_user(username: &str) -> Result<bool> {
+    // open ALLOWED_USERS_FILE, return err if not exists
+    match fs::read_to_string(ALLOWED_USERS_FILE) {
+        Ok(users_file) => {
+            for line in users_file.lines() {
+                if username == line.trim() {
+                    return Ok(true);
                 }
+            }
+        }
+        Err(err) => {
+            return err!(
+                "Unable to read allowed users file {}: {err}",
+                ALLOWED_USERS_FILE
+            );
+        }
+    }
+    Ok(false)
+}
+
+fn get_msg(update: &Update) -> Result<&Message> {
+    match &update.kind {
+        UpdateKind::Message(msg) | UpdateKind::EditedMessage(msg) => Ok(msg),
+        _ => {
+            err!("Nothing to update today")
+        }
+    }
+}
+
+fn get_user(msg: &Message) -> Result<User> {
+    match &msg.kind {
+        Common(message_common) => {
+            let user = message_common
+                .from
+                .clone()
+                .expect("Unable to find user in message");
+            Ok(user)
+        }
+        _ => {
+            err!("Not a common type message")
+        }
+    }
+}
+
+fn is_day_msg(msg: &Message) -> Result<Day> {
+    match &msg.kind {
+        Common(msg_common) => {
+            if let Text(txt) = &msg_common.media_kind {
+                let day: Day = txt.text.parse()?;
+                return Ok(day);
             }
             err!("Update is not Message or EditedMessage")
         }
@@ -136,9 +205,9 @@ fn is_day_msg(update: &Update) -> Result<Day> {
     }
 }
 
-async fn print_day_msg(bot: Bot, day: &Day) -> Result<()> {
-    let _msg = bot
-        .send_message(Id(CHAT_ID), format!("{}", day).to_string())
-        .await?;
-    Ok(())
-}
+// async fn print_day_msg(bot: Bot, day: &Day) -> Result<()> {
+//     let _msg = bot
+//         .send_message(Id(CHAT_ID), format!("{}", day).to_string())
+//         .await?;
+//     Ok(())
+// }
--- vision.md
+++ vision.md
@@ -1,1 +1,2 @@
 1. there's a whitelist, if user is not on whitelist message is ignored
+2. Bot only saves Done section each day on TXT file
